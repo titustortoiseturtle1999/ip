ba3cdbb6c7f4267981edf445d1636a05624c5533
titustortoiseturtle1999 <ngtitus@ymail.com>
2021-08-13 21:53:18 +0800

    add greet function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
 public class Duke {
+    public static void greet() {
+        System.out.println("------------------------------");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can i do for you?");
+        System.out.println("------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +13,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
