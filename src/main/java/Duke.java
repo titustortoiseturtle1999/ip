5b02000a029908dbe7fda1fcf7003984fc428912
TITUSG15\Titus <ngtitus@ymail.com>
2021-08-23 16:52:05 +0800

    Include echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void task1EchoCommands() {
+        Scanner in = new Scanner(System.in);
+        String line = "";
+        while (!line.equals("bye")) {
+            System.out.println("Enter a command: \nTo exit, enter \"bye\"" );
+            line = in.nextLine();
+            System.out.println(line);
+        }
+    }
     public static void greet() {
+
         System.out.println("------------------------------");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can i do for you?");
+        System.out.println("I complete tasks and find imposters! \nHow can i help you?");
         System.out.println("------------------------------");
+        System.out.println("My first task is to echo your commands!");
+        task1EchoCommands();
         System.out.println("Bye. Hope to see you again soon!");
     }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "........................................\n"
+                + ".............@@@@@@@@@@@@@..............\n"
+                + ".........@@@@@&%%%%%%%%%@@@@@...........\n"
+                + ".......@@@@%%%%%%%%%%%%%%%%@@@..........\n"
+                + "...&&&&&&&&&&&&&&&&&&&%%%%%%@@@.........\n"
+                + "..&&&       ,,,,,,,((&&%%%%%&@@@........\n"
+                + ".&&&,,,,,,,,,,,,,,,((&&&%%%%&@@@@@@@@@..\n"
+                + ".(&&(((((,,,/((((((((&&%%%%%&&@@%%%%@@@.\n"
+                + "...&&&&((((((((((((&&&&%%%%%&&@@&&&&&@@.\n"
+                + "...,@@&&&&&&&&&&&&&&%%%%%%%%&&@@&&&&&@@.\n"
+                + "...&@@%%%%%%%%%%%%%%%%%%%%%%&&@@&&&&&@@@\n"
+                + "...@@@%%%%%%%%%%%%%%%%%%%%%&&&@@&&&&&@@@\n"
+                + "...&@@&%%%%%%%%%%%%%%%%%%%&&&&@@&&&&&@@@\n"
+                + "....@@&&&%%%%%%%%%%%%%%%&&&&&&@@&&&&&@@@\n"
+                + "....@@&&&&&&&&&&&&&&&&&&&&&&&&@@&&&&&@@.";
+        System.out.println("Hello! I'm Amon Gus");
+        System.out.println("------------------------------");
+        System.out.println(logo);
         greet();
     }
 }
