73dd11d77aa6b8150a8bebebdecf5e607df0318b
titustortoiseturtle1999 <ngtitus@ymail.com>
2021-08-23 23:22:15 +0800

    add list and help commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,50 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
-    public static void task1EchoCommands() {
+    public static String[] append(String[] list, String item) {
+        String[] newList = Arrays.copyOf(list, list.length+1);
+        newList[list.length] = item;
+        return newList;
+    }
+
+    public static void printTasks(String[] Tasks) {
+        for (int i = 0; i < Tasks.length; i++) {
+            System.out.println(i+1 + ". " + Tasks[i]);
+        }
+    }
+
+    public static void showCommands() {
+        System.out.println("bye: exit program");
+        System.out.println("list: list your tasks");
+    }
+    public static String[] handleCmd(String line, String[] Tasks) {
+        if (line.equals("list")) {
+            printTasks(Tasks);
+        } else if (line.equals("help")) {
+            showCommands();
+        } else if (!line.equals("bye")) {
+            Tasks = append(Tasks, line);
+            System.out.println("new task assigned to you: " + line);
+        }
+        return Tasks;
+    }
+    public static void task1EchoAndStore() {
         Scanner in = new Scanner(System.in);
         String line = "";
+        String[] Tasks = new String[]{};
         while (!line.equals("bye")) {
-            System.out.println("Enter a command: \nTo exit, enter \"bye\"" );
+            System.out.println("Enter a Task: \nTo view my commands, type \"help\"");
             line = in.nextLine();
-            System.out.println(line);
+            Tasks = handleCmd(line, Tasks);
         }
     }
     public static void greet() {
-
         System.out.println("------------------------------");
         System.out.println("I complete tasks and find imposters! \nHow can i help you?");
         System.out.println("------------------------------");
-        System.out.println("My first task is to echo your commands!");
-        task1EchoCommands();
+        System.out.println("My first task is to echo your Tasks!");
+        task1EchoAndStore();
         System.out.println("Bye. Hope to see you again soon!");
     }
     public static void main(String[] args) {
